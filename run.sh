cf110a193a5180aef267d9912e37dc1800f177b9
Craig Miller <cmiller@radiantlogic.com>
2025-11-24 10:26:53 -0500

    Progress

--- run.sh
+++ run.sh
@@ -2,6 +2,13 @@
 
 set -euo pipefail
 
+check_for_env_files() {
+  if [ ! -f .env-server ]; then
+    echo "Missing .env-server file with server-specific environment variables" >&2
+    exit 1
+  fi
+}
+
 get_command() {
   if [ $# -ne 1 ]; then
     echo "Must specify command: start, stop, or setup" >&2
@@ -23,6 +30,7 @@ setup() {
   echo "TBD"
 }
 
+check_for_env_files
 command="$(get_command "$@")"
 
 case "$command" in
