80008555535d1f6a6ddb47d9beeeb1a66fb77d1a
Craig Miller <cmiller@radiantlogic.com>
2025-11-24 14:54:23 -0500

    progress

--- templates/iddm-promotion-datasource-DATABASE.sh
+++ templates/iddm-promotion-datasource-DATABASE.sh
@@ -1,7 +1,6 @@
 # Do not change CATEGORY, it determines that we are using a database
 export CATEGORY=database
-export TYPE=PostgreSQL
+export NAME=craigpg
 export JDBC_URL=jdbc:postgresql://192.168.7.232:30019/foobar
 export USERNAME=
 export PASSWORD=
-export DRIVER_CLASS_NAME=org.postgresql.Driver
