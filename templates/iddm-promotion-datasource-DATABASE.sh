3f9e29b582c36452b5fe3506777b59202d8368f0
Craig Miller <cmiller@radiantlogic.com>
2025-11-24 14:56:46 -0500

    Progress

--- templates/iddm-promotion-datasource-DATABASE.sh
+++ templates/iddm-promotion-datasource-DATABASE.sh
@@ -1,6 +1,5 @@
 # Do not change CATEGORY, it determines that we are using a database
 export CATEGORY=database
-export NAME=craigpg
 export JDBC_URL=jdbc:postgresql://192.168.7.232:30019/foobar
 export USERNAME=
 export PASSWORD=
