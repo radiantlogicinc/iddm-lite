0c1bc84c0f99ceab0a1c757589c9156081121bf6
Craig Miller <cmiller@radiantlogic.com>
2025-11-24 14:52:37 -0500

    Progress

---
+++ templates/iddm-promotion-datasource-DATABASE.sh
@@ -0,0 +1,7 @@
+# Do not change CATEGORY, it determines that we are using a database
+export CATEGORY=database
+export TYPE=PostgreSQL
+export JDBC_URL=jdbc:postgresql://192.168.7.232:30019/foobar
+export USERNAME=
+export PASSWORD=
+export DRIVER_CLASS_NAME=org.postgresql.Driver
--- templates/iddm-promotion-git.sh
+++ templates/iddm-promotion-git.sh
@@ -1,3 +1,3 @@
-export GIT_REPO=
+export GIT_REPO=git@gitlab.com:someone/auto-promotion-data.git
 export GIT_SSH_KEY_BASE64=
 export GIT_BRANCH=
